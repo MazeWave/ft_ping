11c346de2d347b1744a45380548c9a5e7a47d17c
MazeWave <louisdalmasso2003@gmail.com>
2026-01-09 14:06:29 +0100

    Refactor help function to return EXIT_FAILURE on error, update logging in init_ping_struct, and adjust function return type for clarity.

---
+++ includes/parser.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parser.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ldalmass <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/09 14:03:40 by ldalmass          #+#    #+#             */
+/*   Updated: 2026/01/09 14:04:45 by ldalmass         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PARSER_H
+# define PARSER_H
+
+# include <stdbool.h>
+# include <stddef.h>
+# include <string.h>
+# include <stdio.h>
+# include <unistd.h>
+# include <stdlib.h>
+
+#endif
