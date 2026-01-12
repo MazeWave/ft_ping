1743b5430e5c34174d653a32e8790b4b4423a5c7
MazeWave <louisdalmasso2003@gmail.com>
2026-01-12 13:36:19 +0100

    Update file headers in main.c and parser.c with new timestamps; add AUTO_LOG calls in print_ping_struct, help, and version functions for improved logging.

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: ldalmass <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/09 11:26:14 by maze              #+#    #+#             */
-/*   Updated: 2026/01/12 13:26:22 by ldalmass         ###   ########.fr       */
+/*   Updated: 2026/01/12 13:35:45 by ldalmass         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,13 +18,12 @@ int	main(int argc, char **argv unused)
 
 	if (argc < 2)
 		return (help(argv[0]), EXIT_FAILURE);
-	
+
 	t_ping	pingu;
 	t_ping	*ping = &pingu;
-    
+
 	init_ping_struct(ping, argv);
 	parse_args(argc, argv, ping);
-
 	print_ping_struct(ping);
 	return (0);
 }
--- src/parser.c
+++ src/parser.c
@@ -6,7 +6,7 @@
 /*   By: ldalmass <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/09 14:35:54 by ldalmass          #+#    #+#             */
-/*   Updated: 2026/01/12 13:28:48 by ldalmass         ###   ########.fr       */
+/*   Updated: 2026/01/12 13:35:57 by ldalmass         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 
 void    print_ping_struct(t_ping *ping)
 {
+	AUTO_LOG;
 	LOG(BLUE);
 	LOG("is_bonus: %d", ping->is_bonus);
 	LOG("is_root: %d", ping->is_root);
@@ -24,11 +25,13 @@ void    print_ping_struct(t_ping *ping)
 
 void	help(char *elf_name)
 {
+	AUTO_LOG;
 	printf("Usage: %s <hostname>\n", elf_name);
 }
 
 void	version(void)
 {
+	AUTO_LOG;
 	LOG(GREEN "ft_ping -- ldalmass -- version: 1.0.0" RESET);
 }
 void	init_ping_struct(t_ping *ping, char **argv)
@@ -43,25 +46,25 @@ void	init_ping_struct(t_ping *ping, char **argv)
 
 int parse_args(int argc, char **argv, t_ping *ping)
 {
-    AUTO_LOG;
-    // get the options
-    int opt = 0;
+	AUTO_LOG;
+	// get the options
+	int opt = 0;
 	while ((opt = getopt(argc, argv, "hvc:")) != -1)
 	{
 		switch (opt)
 		{
-            case 'c':
-                ping->count = atoi(optarg);
-                break;
-            case 'v':
-                return (version(), EXIT_SUCCESS);
+			case 'c':
+				ping->count = atoi(optarg);
+				break;
+			case 'v':
+				return (version(), EXIT_SUCCESS);
 			case 'h':
 				return (help(argv[0]), EXIT_SUCCESS);
 		}
 	}
-    // check a hostname is provided
-    if (optind != argc - 1)
-        return (help(argv[0]), EXIT_FAILURE);
-    ping->hostname = argv[optind];
-    return (EXIT_SUCCESS);
+	// check if a hostname is provided
+	if (optind != argc - 1)
+		return (help(argv[0]), EXIT_FAILURE);
+	ping->hostname = argv[optind];
+	return (EXIT_SUCCESS);
 }
